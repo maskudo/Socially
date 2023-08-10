fd52ca3548c37b2d9b877bfbd65bf965414197e1
maskudo <manujkarki101@gmail.com>
2023-08-10 16:47:16 +0545

    feat: add posts and fix profile pic

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,42 +1,65 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import Post from '../components/common/Post';
 import COLORS from '../constants/colors';
 import TYPOGRAPHY from '../constants/typography';
+import {POST} from '../utils/data';
 export default function Profile() {
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.blueContainer} />
-      <View style={styles.profile}>
-        <View style={styles.imageContainerOutline}>
-          <View style={styles.innerImageContainer}>
-            <Image
-              source={require('../../assets/img/profile/face.jpg')}
-              style={styles.profileImage}
-              resizeMode="cover"
-            />
+      <FlatList
+        style={styles.postsContainer}
+        data={[1, 2, 3, 4, 5]}
+        stickyHeaderHiddenOnScroll={true}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => <Post post={POST} />}
+        keyExtractor={item => item}
+        ListHeaderComponent={
+          <View>
+            <View style={styles.profile}>
+              <View style={styles.imageContainerOutline}>
+                <View style={styles.imageContainer}>
+                  <Image
+                    source={require('../../assets/img/profile/face.jpg')}
+                    style={styles.profileImage}
+                    resizeMode="cover"
+                  />
+                </View>
+              </View>
+              <Text style={styles.name}>Jane Doe</Text>
+              <Text style={styles.handle}>@janedoe</Text>
+            </View>
+            <View style={styles.statistics}>
+              <View>
+                <Text style={styles.stat}>Posts</Text>
+                <Text style={styles.number}>35</Text>
+              </View>
+              <View>
+                <Text style={styles.stat}>Followers</Text>
+                <Text style={styles.number}>123</Text>
+              </View>
+              <View>
+                <Text style={styles.stat}>Follows</Text>
+                <Text style={styles.number}>654</Text>
+              </View>
+            </View>
+            <View style={styles.postIcons}>
+              <Feather name={'image'} size={25} color={COLORS.black} />
+              <Feather name={'bookmark'} size={25} color={COLORS.black} />
+            </View>
           </View>
-        </View>
-        <Text style={styles.name}>Jane Doe</Text>
-        <Text style={styles.handle}>@janedoe</Text>
-      </View>
-      <View style={styles.statistics}>
-        <View>
-          <Text style={styles.stat}>Posts</Text>
-          <Text style={styles.number}>35</Text>
-        </View>
-        <View>
-          <Text style={styles.stat}>Followers</Text>
-          <Text style={styles.number}>123</Text>
-        </View>
-        <View>
-          <Text style={styles.stat}>Follows</Text>
-          <Text style={styles.number}>654</Text>
-        </View>
-      </View>
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    position: 'relative',
+  },
   profile: {
     height: 250,
     width: '100%',
@@ -64,19 +87,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     ...TYPOGRAPHY.h2Bold,
   },
-  posts: {},
   profileImage: {
     position: 'absolute',
-    width: '100%',
-    height: '100%',
-  },
-  innerImageContainer: {
-    position: 'relative',
     transform: [{rotate: '-45deg'}],
-    width: 80,
-    height: 80,
-    borderRadius: 20,
+    width: 120,
+    height: 120,
+  },
+  imageContainer: {
+    position: 'relative',
+    width: 90,
+    height: 90,
+    borderRadius: 35,
     overflow: 'hidden',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   imageContainerOutline: {
     width: 100,
@@ -85,6 +110,15 @@ const styles = StyleSheet.create({
     borderRadius: 35,
     transform: [{rotate: '45deg'}],
     padding: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  postIcons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
   },
   blueContainer: {
     position: 'absolute',
@@ -97,5 +131,8 @@ const styles = StyleSheet.create({
     borderColor: COLORS.lightgray,
     backgroundColor: COLORS.lightblue,
     transform: [{rotate: '-45deg'}],
+  },
+  postsContainer: {
+    display: 'flex',
   },
 });
---
+++ src/utils/data.tsx
@@ -0,0 +1,8 @@
+const POST = {
+  createdAt: '2 Hours ago',
+  createBy: 'Dennis Reynolds',
+  comments: ['hello', 'world'],
+  likes: ['apar', 'praful'],
+  saves: ['apar', 'praful'],
+};
+export {POST};
